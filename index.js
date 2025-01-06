7b4c0c4e1cdf47789c9522eac61abcf9ff90e802
MrJokerz007 <166495489+MrJokerz007@users.noreply.github.com>
2025-01-06 21:31:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": "8c63dbce-80a7-455a-890b-9368d16e1dcb"}}
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.responce.data);
+  }
+});
+
+app.listen(3001);
